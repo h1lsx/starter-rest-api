449f09ce05b3a95bc28f070da42ba55c1db60fc8
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-10-26 19:48:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 // app.use(express.static('public', options))
 // #############################################################################
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "templeaderboard.henryliu41.repl.co"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
