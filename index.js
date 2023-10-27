592fcc5d81be970d08ca8eee1397947accf4efae
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-10-26 20:52:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ app.get('/:col', async (req, res) => {
   console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
   const items = await db.collection(col).list()
   console.log(JSON.stringify(items, null, 2))
+  res.send(items["responseJSON"]["results"])
   res.json(items).end()
 })
 
